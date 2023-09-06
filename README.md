360ea8a122997b1cc274d357f31b1a98eb0cc2c5
AnthonyGrullonA <anthonygrulon283@gmail.com>
2023-09-05 21:31:36 -0400

    Project

---
+++ README.md
@@ -0,0 +1,7 @@
+echo "# cpsc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AnthonyGrullonA/cpsc.git
+git push -u origin main
---
+++ cpsc/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cpsc project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cpsc.settings')
+
+application = get_wsgi_application()
