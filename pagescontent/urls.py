918dc756edaff87f8a7c9c1c153c0b1c912c2c31
Jairol Anthony Grullon Amparo <jgrullon@napdelcaribe.com.do>
2023-09-15 10:09:12 -0400

    Templates

--- pagescontent/urls.py
+++ pagescontent/urls.py
@@ -2,8 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="Home"),
-    path('Dashboard/', views.adminlte, name="Dashboard"),
-    path('DashboardV/', views.adminvacio, name='PRUEBA 2')
+#Prueba de plantillas AdminLTE (Despues cambiar nombre de funciones)   
+    path('DashboardV/', views.adminvacio, name='PRUEBA 2'),
+###########################################################################
+    path('', views.Home, name="Home"),
+    path('Dashboard/', views.Admin_Page_Service_Center, name="Dashboard"),
+    path('industry/', views.Industry_View, name="Industry"),
+    path('service/', views.Services_View, name='Services'),
+    path('ClientContact/', views.Client_Contact_View, name='ClientContact'),
+    path('ClientList/', views.Client_List_View, name='ClientList'),
+    path('ServiceOrder/', views.Service_Order_VIew, name='ServiceOrder'),
+    path('ConduceEntrada/', views.Input_Conduce_View, name='ConduceEntrada'),
+    path('ConduceSalida', views.Output_Conduce_View, name='ConduceSalida'),
+    path('acl/', views.Acl_Permanent_View, name='ACL'),
+    path('acl_temp/', views.Acl_Temporary_View, name='ACL_TEMP'),
+    path('Clientes_R/', views.Restridted_Client_View, name='ClientesRestringidos'),
+    path('Calendario/', views.Calendar_view, name='Calendario'),
+    path('Task/', views.Task_List_View, name='TaskList'),
     
 ]
--- pagescontent/views.py
+++ pagescontent/views.py
@@ -3,12 +3,64 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 # Create your views here.
 
-def home(request):
+#El home carga el login 1
+def Home(request):
     return render(request, 'home.html')
 
+
+def Admin_Page_Service_Center(request):
+    return render(request, 'admin_template/index.html')
+
+
+def Industry_View(request):
+    return render(request, 'pages/bank_info/industry.html')
+
+
+def Services_View(request):
+    return render(request, 'pages/bank_info/service.html')
+
+
+def Client_Contact_View(request):
+    return render(request, 'pages/client_control/client_contact.html')
+
+
+def Client_List_View(request):
+    return render(request, 'pages/client_control/client_list.html')
+
+
+def Service_Order_VIew(request):
+    return render(request, 'pages/client_control/service_order.html')
+
+
+def Input_Conduce_View(request):
+    return render(request, 'pages/conduces/entrada.html')
+
+
+def Output_Conduce_View(request):
+    return render(request, 'pages/conduces/salida.html')
+
+
+def Acl_Permanent_View(request):
+    return render(request, 'pages/cosmos/acl_permanent.html')
+
+
+def Acl_Temporary_View(request):
+    return render(request, 'pages/cosmos/acl_temporary.html')
+
+
+def Restridted_Client_View(request):
+    return render(request, 'pages/cosmos/restridted_client.html')
+
+
+def Calendar_view(request):
+    return render(request, 'pages/time_c/calendar.html')
+
+
+def Task_List_View(request):
+    return render(request, 'pages/time_c/task_list.html')
+
 #Prueba de plantillas AdminLTE (Despues cambiar nombre de funciones)
-def adminlte(request):
-    return render(request, ('admin_template/index.html'))
 
 def adminvacio(request):
     return render(request, 'admin_template/starter.html')
+
